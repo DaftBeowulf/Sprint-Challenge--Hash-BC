83ac54a9a50fa9f759b079400114e2e0794408d1
DaftBeowulf <ajmboosh@gmail.com>
2019-12-06 12:45:22 -0600

    :sparkles: ex2 finished, passes all tests
    Found source of bug: was looking for genesis ticket with source of None instead of NONE

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,15 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+        if ticket.source == "NONE":
+            route[0] = ticket.destination
+    
+    curr_ticket = route[0]
+    pointer = 1
+    while route[-1] is None:
+        route[pointer] = hash_table_retrieve(hashtable, curr_ticket)
+        curr_ticket = route[pointer]
+        pointer += 1
     return route
